89d96fe30885a0c27ec86cf5057a66dcd195d55e
NG Zheng Wei <nzhengwei99@gmail.com>
2021-10-18 23:25:43 +0800

    test

--- game.c
+++ game.c
@@ -1,32 +1,13 @@
 #include "cprocessing.h"
 
-#define CIRCLE_SIZE 30
-float gCirclePositionX;
-float gCirclePositionY;
-
 void game_init(void)
 {
-	gCirclePositionX = 0.f;
-	gCirclePositionY = 0.f;
+
 }
 
 void game_update(void)
 {
-	if (CP_Input_MouseTriggered(MOUSE_BUTTON_1)) {
-		gCirclePositionX = CP_Input_GetMouseX();
-		gCirclePositionY = CP_Input_GetMouseY();
-	}
-	CP_Graphics_ClearBackground(CP_Color_Create(0, 0, 0, 255));
-	CP_Settings_Fill(CP_Color_Create((int)gCirclePositionX,(int)gCirclePositionY,0,255));
-	CP_Graphics_DrawCircle(gCirclePositionX, gCirclePositionY, CIRCLE_SIZE);
-	/********************************************************
-	 * TODO: Your implementation here. Steps are as follows:
-	 * 1. Update gCirclePosition based on input
-	 * 2. Clear the background to any color you choose.
-	 * 3. Draw a circle:
-	 *    - Its position should be gCirclePosition's x,y value
-	 *    - Its radius should be CIRCLE_SIZE
-	 *******************************************************/
+
 }
 
 
--- main.c
+++ main.c
@@ -1,22 +1,6 @@
-/*---------------------------------------------------------
- * file:	main.c
- * author:	[NAME]
- * email:	[DIGIPEN EMAIL ADDRESS]
-* 
- * brief:	Main entry point for the sample project
-			* of the CProcessing library
-* 
- * documentation link:
- * https://inside.digipen.edu/main/GSDP:GAM100/CProcessing
-* 
- * Copyright © 2020 DigiPen, All rights reserved.
-* ---------------------------------------------------------*/
 #include <stdio.h>
 #include "cprocessing.h"
 
-
-/* Forward declarations */
-extern CP_Vector gCirclePosition;
 void game_init(void);
 void game_update(void);
 void game_exit(void);
