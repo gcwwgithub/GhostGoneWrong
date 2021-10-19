e1fd91f34b74074560fca8c6bc19147ed03d95e3
NG Zheng Wei <nzhengwei99@gmail.com>
2021-10-19 15:06:19 +0800

    Variables Updated

--- game.c
+++ game.c
@@ -3,14 +3,17 @@
 #define grid_Cols 10
 #define grid_Rows 10
 
+#define color_Black  CP_Color_Create(255, 255, 255, 255)
+#define color_Grey CP_Color_Create(128, 128, 128, 255)
+
+float gridRowsLength, gridColsLength;
 void grid(void);
 
 void game_init(void)
 {
-	CP_Color colorBlack = CP_Color_Create(255, 255, 255, 255), colorGrey = CP_Color_Create(128, 128, 128, 255);
-	float gridRowsLength = CP_System_GetDisplayWidth() / grid_Rows, gridColsLength = CP_System_GetDisplayHeight() / grid_Cols;
-	extern colorBlack, colorGrey, gridRowsLength, gridColsLength;
 	CP_System_Fullscreen();
+	gridRowsLength = CP_System_GetDisplayWidth() / grid_Rows;
+	gridColsLength = CP_System_GetDisplayHeight() / grid_Cols;
 }
 
 void game_update(void)
