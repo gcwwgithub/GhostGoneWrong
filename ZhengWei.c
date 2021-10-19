7683cc9f6893ee7de9a032bd28572d30cb397630
NG Zheng Wei <nzhengwei99@gmail.com>
2021-10-19 15:01:41 +0800

    Declared Variable

--- ZhengWei.c
+++ ZhengWei.c
@@ -1,1 +1,9 @@
 #include "cprocessing.h"
+
+void grid(void)
+{
+	
+	CP_Graphics_ClearBackground(colorGrey);
+	CP_Settings_Fill(colorBlack);
+	CP_Graphics_DrawLine(0, 0, 400, 400);
+}
--- game.c
+++ game.c
@@ -1,13 +1,20 @@
 #include "cprocessing.h"
 
+#define grid_Cols 10
+#define grid_Rows 10
+
+void grid(void);
+
 void game_init(void)
 {
-
+	CP_Color colorBlack = CP_Color_Create(255, 255, 255, 255), colorGrey = CP_Color_Create(128, 128, 128, 255);
+	float gridRowsLength = CP_System_GetDisplayWidth() / grid_Rows, gridColsLength = CP_System_GetDisplayHeight() / grid_Cols;
+	extern colorBlack, colorGrey, gridRowsLength, gridColsLength;
+	CP_System_Fullscreen();
 }
 
 void game_update(void)
 {
-
 }
 
 
