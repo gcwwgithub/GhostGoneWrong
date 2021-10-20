e92d4577098fa4f4e143349a4745271029c93cdd
recon68 <42729894+recon68@users.noreply.github.com>
2021-10-20 16:54:19 +0800

    minor stuff + header file

--- Anderson.c
+++ Anderson.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
 #include "cprocessing.h"
 #include "game.h"
 
 CP_Font debugSquareFont;
+
+ObjectData pauseButton;
+
 int redSquareClicked = 0;
 int blueSquareClicked = 0;
 int whiteSquareClicked = 0;
 int greySquareClicked = 0;
+
+int turretButton0Clicked = 0;
+int turretButton1Clicked = 0;
+int turretButton2Clicked = 0;
+int turretButton3Clicked = 0;
 
 // Taking top left & bottom right corners.
 int withinBoundaries(float minX, float minY, float maxX, float maxY)
@@ -22,9 +29,10 @@ int withinBoundaries(float minX, float minY, float maxX, float maxY)
 }
 
 // This should output the color of the square that is clicked.
-// This is somewhat hard coded to the grid layout.
-void click_on_square(void)
+// This is dependent on the level set input.
+void click_on_square(LevelData level)
 {
+	int color = 4; // max value of GridState + 1
 	if (CP_Input_MouseTriggered(MOUSE_BUTTON_1))
 	{
 		whiteSquareClicked = 0;
@@ -32,21 +40,37 @@ void click_on_square(void)
 		redSquareClicked = 0;
 		blueSquareClicked = 0;
 
+		turretButton0Clicked = 0;
+		turretButton1Clicked = 0;
+		turretButton2Clicked = 0;
+		turretButton3Clicked = 0;
+
 		float distFromXOriginToMouseX = CP_Input_GetMouseX() - Game.xOrigin;
 		float distFromYOriginToMouseY = CP_Input_GetMouseY() - Game.yOrigin;
 
 		float clickedCol = distFromXOriginToMouseX / Game.gridWidth;
 		float clickedRow = distFromYOriginToMouseY / Game.gridHeight;
 
-		// Only able to account for straight paths
+		// clicked within the grid
 		if ((clickedCol >= 0 && clickedCol < GAME_GRID_COLS) && (clickedRow >= 0 && clickedRow < GAME_GRID_ROWS))
 		{
-			int color = Tutorial.gridColor[(int)clickedRow][(int)clickedCol];
+			color = level.gridColor[(int)clickedRow][(int)clickedCol];
 			switch (color)
 			{
 			case (Grid_Color_White):
 			{
 				whiteSquareClicked = 1;
+				// if (playerIsBuildingTurret) // i.e. clicked on turret to buy and now hovering over this grid cell
+				//		if  // there are no more white-colored grid cells left. || (!turret_space_occupied) 
+				//			// put new turret at center of white grid cell.
+				//			// and remove the cost of said turret from current points
+				//		else // display error message
+				//			
+
+				// else if (turret_space_occupied) // there is turret here
+				//		// select turret - maybe put something on it as visual feedback
+				//		// enable turret window,upgrade menu,etc.
+
 				break;
 			}
 			case (Grid_Color_Grey):
@@ -65,63 +89,90 @@ void click_on_square(void)
 				break;
 			}
 			}
-
 		}
-
-
-		// Tutorial level
-		// If click was within red square's two corners ...
-		//if (withinBoundaries(Game.xOrigin + Game.gridWidth, Game.yOrigin, Game.xOrigin + Game.gridWidth * 2, Game.yOrigin + Game.gridHeight))
-		//{
-		//	redSquareClicked = 1;
-		//}
-		// ... or the blue square's ...
-		//if (withinBoundaries(Game.xOrigin + Game.gridWidth, Game.yOrigin + Game.gridHeight * (Grid_Rows - 1), Game.xOrigin + Game.gridWidth * 2, Game.yOrigin + Game.gridHeight * Grid_Rows))
-		//{
-		//	blueSquareClicked = 1;
-		//}
-
-		//  White area boundaries
-		//if (withinBoundaries(Game.xOrigin, Game.yOrigin, Game.xOrigin + Game.gridWidth, Game.yOrigin + Game.gridHeight * Grid_Rows) ||
-		//	withinBoundaries(Game.xOrigin + Game.gridWidth * 2, Game.yOrigin, Game.xOrigin + Game.gridWidth * 3, Game.yOrigin + Game.gridHeight * Grid_Rows))
-		//{
-		//	whiteSquareClicked = 1;
-		//}
-		//if (withinBoundaries(Game.xOrigin + Game.gridWidth, Game.yOrigin + Game.gridHeight, Game.xOrigin + Game.gridWidth * (Grid_Cols - 1), Game.yOrigin + Game.gridHeight * (Grid_Rows - 1)))
-		//{
-		//	greySquareClicked = 1;
-		//}
+		// if clicked outside the grid
+		else if (withinBoundaries(TurretButton0.xOrigin, TurretButton0.yOrigin, TurretButton0.xOrigin + TurretButton0.width, TurretButton0.yOrigin + TurretButton0.height))
+		{
+			turretButton0Clicked = 1;
+		}
+		else if (withinBoundaries(TurretButton1.xOrigin, TurretButton1.yOrigin, TurretButton1.xOrigin + TurretButton1.width, TurretButton1.yOrigin + TurretButton1.height))
+		{
+			turretButton1Clicked = 1;
+		}
+		else if (withinBoundaries(TurretButton2.xOrigin, TurretButton2.yOrigin, TurretButton2.xOrigin + TurretButton2.width, TurretButton2.yOrigin + TurretButton2.height))
+		{
+			turretButton2Clicked = 1;
+		}
+		else if (withinBoundaries(TurretButton3.xOrigin, TurretButton3.yOrigin, TurretButton3.xOrigin + TurretButton3.width, TurretButton3.yOrigin + TurretButton3.height))
+		{
+			turretButton3Clicked = 1;
+		}
 	}
 
 	debugSquareFont = GAME_FONT;
 	CP_Font_Set(debugSquareFont);
+	CP_Settings_Fill(COLOR_RED);
 	CP_Settings_TextSize(32.0f);
 	if (redSquareClicked)
 	{
-		CP_Font_DrawText("Red square", 100.0f, 100.0f);
+		CP_Font_DrawText("Red square", CP_System_GetWindowWidth() * 0.9f, CP_System_GetWindowHeight() * 0.1f);
 	}
 	if (blueSquareClicked)
 	{
-		CP_Font_DrawText("Blue square", 100.0f, 100.0f);
+		CP_Font_DrawText("Blue square", CP_System_GetWindowWidth() * 0.9f, CP_System_GetWindowHeight() * 0.1f);
 	}
 	if (whiteSquareClicked)
 	{
-		CP_Font_DrawText("White square", 100.0f, 100.0f);
+		CP_Font_DrawText("White square", CP_System_GetWindowWidth() * 0.9f, CP_System_GetWindowHeight() * 0.1f);
 	}
 	if (greySquareClicked)
 	{
-		CP_Font_DrawText("Grey square", 100.0f, 100.0f);
+		CP_Font_DrawText("Grey square", CP_System_GetWindowWidth() * 0.9f, CP_System_GetWindowHeight() * 0.1f);
+	}
+
+	if (turretButton0Clicked)
+	{
+		CP_Font_DrawText("Turret button 0", CP_System_GetWindowWidth() * 0.9f, CP_System_GetWindowHeight() * 0.1f);
+	}
+	else if (turretButton1Clicked)
+	{
+		CP_Font_DrawText("Turret button 1", CP_System_GetWindowWidth() * 0.9f, CP_System_GetWindowHeight() * 0.1f);
+	}	
+	else if (turretButton2Clicked)
+	{
+		CP_Font_DrawText("Turret button 2", CP_System_GetWindowWidth() * 0.9f, CP_System_GetWindowHeight() * 0.1f);
+	}	
+	else if (turretButton3Clicked)
+	{
+		CP_Font_DrawText("Turret button 3", CP_System_GetWindowWidth() * 0.9f, CP_System_GetWindowHeight() * 0.1f);
 	}
 }
 
-// This assumes the pause button's a square.
-void click_on_pause(CP_Image pauseButton, float imagePosX, float imagePosY)
+/*
+void draw_pause_button(float buttonPosX, float buttonPosY)
+{
+	CP_Settings_Fill(COLOR_BLACK);
+	CP_Graphics_DrawRect(buttonPosX, buttonPosY, buttonPosX + 25.0f, buttonPosY + 15.0f);
+	CP_Settings_Fill(COLOR_WHITE);
+	CP_Graphics_DrawRect(buttonPosX * 1.4f, buttonPosY * 1.1f, buttonPosX * 0.25f, 45.0f);
+	CP_Graphics_DrawRect(buttonPosX * 1.9f, buttonPosY * 1.1f, buttonPosX * 0.25f, 45.0f);
+}
+*/
+
+void click_on_pause(float x, float y)
 {
 	if (CP_Input_MouseTriggered(MOUSE_BUTTON_1))
 	{
-		if (withinBoundaries(imagePosX, imagePosY, imagePosX + CP_Image_GetWidth(pauseButton), imagePosY + CP_Image_GetHeight(pauseButton)))
+		//if (withinBoundaries(imagePosX, imagePosY, imagePosX + CP_Image_GetWidth(pauseButton), imagePosY + CP_Image_GetHeight(pauseButton)))
 		{
 			// game is paused.
 		}
 	}
 }
+
+// Terminates game.
+void exit_game(void)
+{
+	CP_Engine_Terminate();
+}
+
