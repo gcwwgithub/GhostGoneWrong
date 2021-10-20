6d86a3b114f7de33edb66fe4fafdfa683e1793cf
John Lim <l.jingruijohn@digipen.edu>
2021-10-20 18:15:19 +0800

    test enemy setup

--- John.h
+++ John.h
@@ -1,3 +1,5 @@
+#include"cprocessing.h"
+#include"game.h"
 typedef enum Direction {
 	NoMove,
 	Up,
@@ -17,18 +19,33 @@ typedef enum EnemyState {
 }EnemyState;
 
 typedef struct Enemy {
-	int health, CurrentWaypoint;
+	int health, CurrentWaypoint, alpha;
 	float posX, posY, enemy_width, enemy_height, angle, speed;
 	EnemyState state;
 	EnemyTypes type;
 }enemy;
 
-void enemy_init(void);
+void enemy_test_init(void);
 void Draw_enemy(enemy* r);
 void enemy_move(enemy* r, float Enemy_PathpointsX[], float Enemy_PathY[], int number_of_points);
 int direction_to_next_point(float enemy_pathpointsX[], float enemy_pathpointsY[], enemy* r);
 int update_point_num(float enemy_pathpointsX[], float enemy_pathpointsY[], enemy* r);
 int Collision(enemy* r, float x, float y);
+void EnemyDeath(enemy* r);
 
 enemy test;
 CP_Image Draw_Red_arrow;
+
+//test path
+float xpoint;
+float ypoint;
+float Xarray[2];
+float Yarray[2];
+
+/* //test enemy movement
+enemy_move(&test, Xarray, Yarray, 2);
+Draw_enemy(&test);
+EnemyDeath(&test);
+
+//test enemy
+enemy_test_init();*/
