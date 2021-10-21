01e8e0b065d97109e5fe9b9c073961edcee38507
recon68 <42729894+recon68@users.noreply.github.com>
2021-10-21 11:24:22 +0800

    play,quit buttons

--- game.c
+++ game.c
@@ -2,6 +2,7 @@
 #include"game.h"
 #include "Samuel.h"
 #include "John.h"
+#include"Anderson.h"
 
 void game_grid_init(void);
 void game_grid_color_init(LevelData* Level);
@@ -12,7 +13,7 @@ void turret1_button_init(void);
 void turret2_button_init(void);
 void turret3_button_init(void);
 void mouse_init(void);
-
+int btn_is_pressed(Coordinates objectButtonX);
 void render_game_grid(void);
 void render_game_grid_color(LevelData Level);
 void render_turret_menu(void);
@@ -21,12 +22,15 @@ void render_button(Coordinates TurretButtonX, CP_Color Color);
 void game_init(void)
 {
 	//CP_System_Fullscreen();
-	CP_System_ShowConsole();
 	currentGameState = MainMenu;
-	
+
+	init_play_button();
+	init_quit_button();
+	init_level_select_buttons();
+
 	//game grid 
 	game_grid_init();
-	
+
 	//turret menu
 	turret_menu_init();
 
@@ -57,29 +61,52 @@ void game_update(void)
 		MouseInput.xOrigin = CP_Input_GetMouseX();
 		MouseInput.yOrigin = CP_Input_GetMouseY();
 	}
+	if (currentGameState == Wave)
+	{
+		update_turret();
+		update_projectile();
 
-	update_turret();
-	update_projectile();
+		render_game_grid();
+		render_game_grid_color(Tutorial);
+		render_turret_menu();
 
-	render_game_grid();
-	render_game_grid_color(Tutorial);
-	render_turret_menu();
+		render_button(PauseButton, COLOR_PURPLE);
+		render_button(TurretButton0, COLOR_WHITE);
+		render_button(TurretButton1, COLOR_WHITE);
+		render_button(TurretButton2, COLOR_WHITE);
+		render_button(TurretButton3, COLOR_WHITE);
 
-	render_button(PauseButton, COLOR_PURPLE);
-	render_button(TurretButton0, COLOR_WHITE);
-	render_button(TurretButton1, COLOR_WHITE);
-	render_button(TurretButton2, COLOR_WHITE);
-	render_button(TurretButton3, COLOR_WHITE);
-
-	render_turret();
-	render_projectile();
+		render_turret();
+		render_projectile();
 
 
-	//test enemy
-	enemy_move(&test, Xarray, Yarray, 2);
-	Draw_enemy(&test);
-	EnemyDeath(&test);
+		//test enemy
+		enemy_move(&test, Xarray, Yarray, 2);
+		Draw_enemy(&test);
+		EnemyDeath(&test);
+	}
+	else if (currentGameState == MainMenu)
+	{
+		if (btn_is_pressed(PlayButton.buttonData))
+		{
+			currentGameState = Wave;
+		}
+		else if (btn_is_pressed(QuitButton.buttonData))
+		{
+			exit_game();
+		}
 
+		CP_Graphics_ClearBackground(COLOR_GREY);
+		render_game_title();
+		render_ui_button(PlayButton);
+		render_ui_button(QuitButton);
+	}
+	else if (currentGameState == LevelSelect)
+	{
+		CP_Graphics_ClearBackground(COLOR_GREY);
+		render_game_title();
+		render_level_select_buttons();
+	}
 }
 void game_exit(void)
 {
