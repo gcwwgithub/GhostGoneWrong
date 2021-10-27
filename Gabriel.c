fc6ec951bad6841833d91de9fa3e218703c9a4d0
gcwwgithub <gabrielchiok@hotmail.com>
2021-10-27 16:54:36 +0800

    New Art

--- Gabriel.c
+++ Gabriel.c
@@ -2,21 +2,21 @@
 #include "Gabriel.h"
 void init_all_images(void)
 {
-	triangleTurretImageArray[0] = CP_Image_Load("./Assets/Triangle1.png");
-	triangleTurretImageArray[1] = CP_Image_Load("./Assets/Triangle2.png");
-	triangleTurretImageArray[2] = CP_Image_Load("./Assets/Triangle3.png");
-	triangleTurretImageArray[3] = CP_Image_Load("./Assets/Triangle4.png");
-	triangleTurretImageArray[4] = CP_Image_Load("./Assets/Triangle5.png");
-	triangleTurretImageArray[5] = CP_Image_Load("./Assets/Triangle6.png");
+	triangleTurretImageArray[0] = CP_Image_Load("./Assets/BasicTurret1.png");
+	triangleTurretImageArray[1] = CP_Image_Load("./Assets/BasicTurret2.png");
+	triangleTurretImageArray[2] = CP_Image_Load("./Assets/BasicTurret3.png");
+	triangleTurretImageArray[3] = CP_Image_Load("./Assets/BasicTurret1.png");
+	triangleTurretImageArray[4] = CP_Image_Load("./Assets/BasicTurret4.png");
+	triangleTurretImageArray[5] = CP_Image_Load("./Assets/BasicTurret5.png");
 
-	redArrowImageArray[0] = CP_Image_Load("./Assets/RedArrow1.png");
-	redArrowImageArray[1] = CP_Image_Load("./Assets/RedArrow2.png");
-	redArrowImageArray[2] = CP_Image_Load("./Assets/RedArrow3.png");
+	redArrowImageArray[0] = CP_Image_Load("./Assets/BasicGhost1.png");
+	redArrowImageArray[1] = CP_Image_Load("./Assets/BasicGhost2.png");
+	redArrowImageArray[2] = CP_Image_Load("./Assets/BasicGhost3.png");
 
 	tempStar = CP_Image_Load("./Assets/Star1.png");
 	tempCircle = CP_Image_Load("./Assets/Circle1.png");
 	tempPercentage = CP_Image_Load("./Assets/Percentage1.png");
-	tempBullet = CP_Image_Load("./Assets/TriangleBullet.png");
+	tempBullet = CP_Image_Load("./Assets/BasicTurretBullet.png");
 	tempBulletRadius = CP_Image_Load("./Assets/BulletRadius.png");
 	bluePortalSpriteSheet = CP_Image_Load("./Assets/SummationPortal.png"); 
 	redPortalSpriteSheet = CP_Image_Load("./Assets/EnemyPortal.png");  
