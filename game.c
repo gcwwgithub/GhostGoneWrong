4ff0d78959f6b3a9bc87ad33afeaf4ef39aae73c
NG Zheng Wei <nzhengwei99@gmail.com>
2021-12-05 14:35:20 +0800

    Added comments

--- game.c
+++ game.c
@@ -10,7 +10,7 @@ All content © 2021 DigiPen Institute of Technology Singapore, all rights reserve
 @course     CSD 1400
 @section    C
 @date       01/12/2021
-@brief    	Contains function and variables that are commmonly used by everyone
+@brief    	Runs the game.
 *//*__________________________________________________________________________*/
 #include"game.h"
 #include "Samuel.h"
